1a910bce7c693e4b9a0f37918da5e4c61bfe935b
Hishengs <819537918@qq.com>
2017-11-07 15:41:33 +0800

    首页修改

--- app/router.js
+++ app/router.js
@@ -1,3 +1,7 @@
 module.exports = app => {
 	app.router.get('/', 'home.index');
+
+	app.router.get(/^docs\/?.*/, async function(){
+		this.ctx.redirect('/static/docs');
+	});
 };
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 cls
 set DEBUG=Heysoo:*,-not_this
-supervisor index.js
+supervisor local.js
