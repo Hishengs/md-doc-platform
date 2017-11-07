779ad3312f5fb28608e5702e6b3dd772b28f7a9e
Hishengs <819537918@qq.com>
2017-11-07 15:44:22 +0800

    add config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const port = process.env.PORT || 8006;
+
+module.exports = {
+  host: '127.0.0.1',
+  port: port,
+  view: {
+    enabled: true,
+    engine: 'nunjucks',
+  },
+  static: {
+    enabled: true,
+    path: 'static', // 路径
+  },
+  logRequestTime: true
+}
